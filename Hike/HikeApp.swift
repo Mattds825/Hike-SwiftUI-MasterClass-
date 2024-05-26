af1d37a0ddaa8f904e0692fa9305c9329d0e5b6a
Matthew Goncalves Da Silva <matthewgdasilva25@gmail.com>
2024-05-26 19:20:39 +0100

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Matt Da Silva on 26/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
